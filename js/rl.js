b67f45df7eae11184b6bf9a039b3c04f8b95982c
Subrat Mallick <m.subrat198470@gmail.com>
2022-09-08 20:46:38 +0530

    added
    former members
    3 pages

--- js/rl.js
+++ js/rl.js
@@ -1,13 +1,22 @@
 {
     {
         const d1 = [
+            "Previous Team • IIEC",
+            "Team of 2021 • IIEC",
+        ];
+        ! function Newline() {
+            const randomLine = Math.floor(Math.random() * d1.length);
+            document.getElementById('rll1').innerHTML = d1[randomLine];
+        }();
+    } {
+        const d1 = [
             "How We Came Upto Here?",
             "This is Us!",
             "Who Built IIEC?",
         ];
         ! function Newline() {
             const randomLine = Math.floor(Math.random() * d1.length);
-            document.getElementById('rll').innerHTML = d1[randomLine];
+            document.getElementById('rll3').innerHTML = d1[randomLine];
         }();
     }
     {
@@ -20,14 +29,5 @@
             document.getElementById('rll2').innerHTML = d1[randomLine];
         }();
     }
-    {
-        const d1 = [
-            "Previous Team • IIEC",
-            "Team of 2021 • IIEC",
-        ];
-        ! function Newline() {
-            const randomLine = Math.floor(Math.random() * d1.length);
-            document.getElementById('rll3').innerHTML = d1[randomLine];
-        }();
-    }
+
 }
